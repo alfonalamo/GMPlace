b395f5a609d1218088245237703d692198e58240
alfonalamo <a.rodrigueza1995@gmail.com>
2026-04-19 09:42:36 +0200

    Cambios ventana no incluidos en commit anterior

--- interfaz/ventana.py
+++ interfaz/ventana.py
@@ -1,21 +1,30 @@
 import tkinter as tk
 from constantes import estilos
-from interfaz.navegador import Navegador, NavegadorVertical
+from interfaz.navegador import Navegador, NavegadorSuperior
 from interfaz.pantallas import Inicio, Combate, Elementos
 
 class Ventana(tk.Tk):
     def __init__(self, controlador, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.controlador = controlador
+        tupla_pantallas = (Inicio, Combate, Elementos)
+        self.pantallas = {}  # Diccionario de pantallas dentro de la ventana
+
+        self.contenedor_principal = tk.Frame(self)
+
+        for Pantalla in tupla_pantallas:
+            pantalla = Pantalla(self.contenedor_principal, self.controlador)
+            self.pantallas[str(pantalla.nombre)] = pantalla
+
+        self.pantalla_activa = "Elementos" # La pantalla que se verá por defecto al inicio
+        self.barra_navegacion = NavegadorSuperior(self, self.controlador, self.pantallas.values())
+
         self.title("GM place")
         self.geometry("600x600")
         self.minsize(200,200)
         self.config(background=estilos.Color.FONDO)
-        self.pantallas ={}
-        self.modo = ""
-        self.controlador = controlador
 
-        navegador = NavegadorVertical(self, self.controlador)
-        navegador.pack(
+        self.barra_navegacion.pack(
             side= tk.TOP,
             fill= tk.X,
             expand= False,
@@ -23,15 +32,18 @@ class Ventana(tk.Tk):
             pady= 0
         )
 
-        self.contenedor_principal = tk.Frame(self)
         self.contenedor_principal.pack(
             side = tk.TOP,
             fill = tk.BOTH,
-            expand= False,
-            padx= 22,
-            pady= 11
+            expand= True,
+            padx= 0,
+            pady= 0
         )
         self.contenedor_principal.configure(background=estilos.Color.FONDO)
         self.contenedor_principal.grid_columnconfigure(0, weight=1)
         self.contenedor_principal.grid_rowconfigure(0, weight=1)
 
+    def carga_pantallas(self, ventana, tupla):
+        for Pantalla in tupla:
+            pantalla = Pantalla(ventana.contenedor_principal, self)
+            ventana.pantallas[str(pantalla.nombre)] = pantalla
